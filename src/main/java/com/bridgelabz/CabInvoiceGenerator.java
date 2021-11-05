402e8c08deab3d213614ea6e772b34fbd64ecc8d
Daminichandrakar <damini.2201@gmail.com>
2021-11-05 16:03:40 +0530

    Added : Test case and code to calculate totalFare and minimumFare

---
+++ src/main/java/com/bridgelabz/CabInvoiceGenerator.java
@@ -0,0 +1,31 @@
+package com.bridgelabz;
+
+/**
+ * Purpose : To simulate Cab Service
+ *
+ * @author DAMINI CHANDRAKAR
+ * @version 11.0.11
+ * @since 2021-11-05
+ */
+public class CabInvoiceGenerator {
+
+	private final int MINIMUM_FARE = 5;
+	private final int COST_PER_KILOMETER = 10;
+	private final int COST_PER_MINUTE = 1;
+
+	/**
+	 * Purpose : calculate totalFare and minimumFare
+	 * 
+	 * @param distance
+	 * @param time
+	 * @return double(totalFare & minimumFare)
+	 */
+	public double calculateFare(double distance, int time) {
+		double totalFare = COST_PER_KILOMETER * distance + COST_PER_MINUTE * time;
+		if (totalFare < MINIMUM_FARE) {
+			return MINIMUM_FARE;
+		}
+		return totalFare;
+	}
+
+}
---
+++ src/test/java/com/bridgelabz/CabInvoiceGeneratorTest.java
@@ -0,0 +1,22 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CabInvoiceGeneratorTest {
+
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		double result = cabInvoiceGenerator.calculateFare(15, 30);
+		Assert.assertEquals(180, result, 0.0);
+	}
+
+	@Test
+	public void givenDistanceAndTime_ShouldReturnMinFare() {
+		CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+		double result = cabInvoiceGenerator.calculateFare(0.1, 1);
+		Assert.assertEquals(5, result, 0.0);
+
+	}
+}
